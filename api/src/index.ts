f5506b7a645c542d3a9f3deff7bbde751c3c8e16
Sergey Abramov <sabramov@insquad.com>
2024-06-14 15:16:47 +0800

    task-3: feat | use invertedIndex for GET /companies

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,17 +3,12 @@ import express from 'express';
 import { faker } from '@faker-js/faker';
 
 import companies from './data/companies.json';
+import { createdInvertedIndex, splitTextToTokens } from './utils/createInvertedIndex';
+import { InvertedIndexType } from './utils/invertedIndex.type';
+import { MIN_TOKEN_LENGTH } from './utils/invertedIndex.constants';
 
-const COINS = [
-  'btc',
-  'eth',
-  'ada',
-  'bnb',
-  'trx',
-  'doge',
-  'dot',
-  'eos',
-];
+const COINS = ['btc', 'eth', 'ada', 'bnb', 'trx', 'doge', 'dot', 'eos'];
+let INVERTED_INDEX_CACHE: InvertedIndexType;
 
 const app = express();
 const port = 3001;
@@ -22,11 +17,24 @@ app.use(cors());
 
 app.get('/companies', (req, res) => {
   const searchQuery = req.query.query as string | undefined;
-  const filteredCompanies = companies.filter((company) =>
-    searchQuery ? company.name.toLowerCase().includes(searchQuery.toLowerCase()) : true,
-  );
+  if (!searchQuery || searchQuery.length < MIN_TOKEN_LENGTH) {
+    return res.send(companies);
+  }
 
-  res.send(filteredCompanies);
+  const tokens = splitTextToTokens(searchQuery);
+  const matchingCompanyIndexes: Set<number> = new Set();
+
+  tokens.forEach((token) => {
+    if (INVERTED_INDEX_CACHE[token]) {
+      INVERTED_INDEX_CACHE[token].forEach((index) => {
+        matchingCompanyIndexes.add(index);
+      });
+    }
+  });
+
+  const filteredCompanies = Array.from(matchingCompanyIndexes).map((index) => companies[index]);
+
+  return res.send(filteredCompanies);
 });
 
 app.get('/companies/random', (_req, res) => {
@@ -39,10 +47,7 @@ app.get('/companies/random', (_req, res) => {
         id,
         name: faker.company.name(),
         logoUrl: `https://picsum.photos/seed/ubit-${id}/200`,
-        coins: faker.helpers.arrayElements(
-          COINS,
-          faker.datatype.number({ min: 1, max: 4 }),
-        ),
+        coins: faker.helpers.arrayElements(COINS, faker.datatype.number({ min: 1, max: 4 })),
       };
     });
 
@@ -50,6 +55,8 @@ app.get('/companies/random', (_req, res) => {
 });
 
 app.listen(port, () => {
+  INVERTED_INDEX_CACHE = createdInvertedIndex(companies);
+
   // eslint-disable-next-line no-console
   console.log(`API listening on port ${port}`);
 });
